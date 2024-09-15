90b082b81b0b1b917be4592227aeca6b74ebcc16
riyadhctg <3258642+riyadhctg@users.noreply.github.com>
2024-09-15 19:47:53 +0800

    term4

--- words.js
+++ words.js
@@ -53,17 +53,44 @@ feast, regularly, roared furiously, seldom, fabulous, invitation card, flapped, 
 
 unit 11-2:
 gifts, exciting, nervous, midnight, tasteless, fireworks, assemble, nonsense, neighbour, exclaimed, goody bag, joyful, celebration, a hearty meal, a bunch of flowers, a dog with two tails
+
+
+
+-- Term 4
+
+Unit-12-1:
+fault, wimp, scary, bench, joggers, useless, sneered, business, amazed, terrifying, appeared, comforting words, keep him company, as timid as a mouse, gave a helping hand
+Dictation: suddenly, pushed, grateful
+Unit-12-2:
+toss, avoid, sibling, ignored, thankful, delighted, friendship, instructions, accidentally, accompany, cried in terror, back and forth, came to his rescue, as gentle as a dove, wonderful performance
+Dictation: best friends, missed, recover
+
+Unit-13-1:
+left, write, away, foggy, breezy, snakes, crying, raining, bracelet, postcard, wet night, watch out, hazy evening, hot afternoon, windy morning
+Dictation: collects, stamps, neatly
+Unit-13-2:
+stars, built, asleep, awake, cookies, helmet, uniform, presents, watched, mountain, gave out, good luck, coming home, thinking of you, waiting for someone
+Dictation: ambition, protect, achieve
+
+Unit-14-1:
+job, oiled, smiled, people, washed, retired, project, children, bicycles, wheelchair, free time, catching up, out of breath, recycling bins, pedalled hard
+Dictation: overjoyed, shiny, nearby
+Unit-14-2:
+wife, near, race, useful, things, lights, trishaw, training, nodded, disappointed, at the park, the next day, colourful flags, did not matter, cheered him on
+Dictation: workshop, repair, customers
+
+
 */
 let words = [
-"nails", "screw", "pocket", "hammer", "magnet", "iron bolts", "magnetic", "newspaper", "paper clips", 
-"skateboard", "cardboard", "tournament", "competition", "a game of cards", "a roll of sticky tape", 
-"rope", "tools", "paint", "team", "needle", "attract", "ladder", "shocked", "experiment", "excitement", 
-"tissue", "mischievous", "a ball of string", "a piece of cloth", "a reel of thread", "help", "arrived", 
-"next time", "hose", "monster", "went pale", "asked", "took out", "gone back", "jungle", "returned", 
-"said to herself", "waited", "marched", "put on my glasses", "find", "safe", "told", "truck", "green", "family", 
-"nothing", "chickens", "tomatoes", "dangerous", "drove up", "has eaten", "don't worry", "call for help", 
-"vegetable garden", "feast", "regularly", "roared furiously", "seldom", "fabulous", "invitation card", "flapped", 
-"thumped", "enjoyed ourselves", "pranced", "frequently", "time on her hands", "annually", "reluctantly", 
-"finger-licking good", "gifts", "exciting", "nervous", "midnight", "tasteless", "fireworks", "assemble", 
-"nonsense", "neighbour", "exclaimed", "goody bag", "joyful", "celebration", "a hearty meal", "a bunch of flowers", "a dog with two tails"
-];
+    "nails", "screw", "pocket", "hammer", "magnet", "iron bolts", "magnetic", "newspaper", "paper clips", 
+    "skateboard", "cardboard", "tournament", "competition", "a game of cards", "a roll of sticky tape", 
+    "rope", "tools", "paint", "team", "needle", "attract", "ladder", "shocked", "experiment", "excitement", 
+    "tissue", "mischievous", "a ball of string", "a piece of cloth", "a reel of thread", "help", "arrived", 
+    "next time", "hose", "monster", "went pale", "asked", "took out", "gone back", "jungle", "returned", 
+    "said to herself", "waited", "marched", "put on my glasses", "find", "safe", "told", "truck", "green", "family", 
+    "nothing", "chickens", "tomatoes", "dangerous", "drove up", "has eaten", "don't worry", "call for help", 
+    "vegetable garden", "feast", "regularly", "roared furiously", "seldom", "fabulous", "invitation card", "flapped", 
+    "thumped", "enjoyed ourselves", "pranced", "frequently", "time on her hands", "annually", "reluctantly", 
+    "finger-licking good", "gifts", "exciting", "nervous", "midnight", "tasteless", "fireworks", "assemble", 
+    "nonsense", "neighbour", "exclaimed", "goody bag", "joyful", "celebration", "a hearty meal", "a bunch of flowers", "a dog with two tails"
+    ];
