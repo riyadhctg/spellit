0c638bc712f7e95e13a11e7696bb1498557af0a6
riyadhctg <3258642+riyadhctg@users.noreply.github.com>
2025-07-26 20:21:28 +0800

    Update words.js

--- words.js
+++ words.js
@@ -1,4 +1,70 @@
 /*
+P3 - Term 3
+# Singapore School Spelling and Dictation Lists
+
+## Spelling Week 2:
+facts, digest, attract, reality, feeding, sunlight, inwards, process, releases, probably, developed, well-known, horror story, unusual plants, unique ways of surviving
+
+## Dictation Words Week 2:
+things that are true, to change food into smaller forms that a living thing can absorb and use, to cause something to come towards something else, good mental or physical condition, act or process of eating or being fed, the light that comes from the sun, towards the inside of, gradual changes that lead towards the inside of a plant, lets go of or loosens, very likely, became stronger grew, Famous, a story in which very frightening and unnatural things happen, plants that are different from plants of the same type in a way that is surprising, interesting, or attractive, one of its kind of ways of staying alive
+
+## Spelling Week 3:
+insect, obtain, absorb, gigantic, weaken, pouches, nutrients, trapdoor, reproduce, freshwater, closed tightly, sweet nectar, toothed edges, tiny hairs at the opening, Venus flytraps have tiny hairs at the opening
+
+## Dictation Words Week 3:
+a type of very small animal with six legs, a body divided into three parts and usually two pairs of wings, to get gain, soak up, to take in or soak up, extremely large, lessen the strength of, a small bag that can be closed, substances in food that our bodies process to enable it to function, a lifting or sliding door opening in a roof, ceiling, or floor, to make more, either by having babies or creating copies, water found in ponds, lakes, rivers, streams, glaciers, sealed without a gap, a liquid made by the flowers of a plant from flowers, leaves have toothed edges for protection, Venus flytraps have tiny hairs at the opening
+
+## Spelling Week 4:
+clear, able to seen and understand, circumstances, objects, or surrounded, slimy, environment, polluted, remains of the environment, record my friend's voice from the race, Water is necessary for life, friends we are playing in the race
+
+## Dictation Words Week 4:
+Plants survive in sunlight, The brown spots on the apple means it is rotten, What your hands to prevent spreading germs, The monster is trapped in the cage, actually we sprouted now, The mouse is trying to pump blood, leaves make up for fading, She was afraid of the dark, Was the dark, The down was making silly actions to get the entrance tickets and you can spend the day viewing some of the rarest plants
+
+## Spelling Week 5:
+icy, unpleasant or disgusting, east, coat, packed, right, shaking, wiggled, morning back and forth or up and down, with much, more, something moving down or to and fro, to attack
+
+## Dictation Words Week 5:
+to pick, to polish, to be real, that is located, to lose, at the back, to make a very rapid short series of rocking or jarring sounds, to bounce, to make very angry or furious, to make very angry, confused, became surprised or alarmed, noise, rolled, The toy is very old, The slime was sicky, The down was making silly actions to get the entrance tickets and you can spend the day viewing some of the rarest plants
+
+## Spelling Week 6:
+poke, attract, scented, dropped, through, believe, aerial, smelling, something that is, accept that something is true, He was afraid of the dark, The ball went through the hoop, She dropped her ice cream when, The screamy cat was very hungry, She strawny fell from behind
+
+## Dictation Words Week 6:
+She used a stick to poke holes in the soil, to push a finger or other pointed object quickly into something or someone, to lift, to put a thing down, He was afraid of the dark, The ball went through the hoop, I believe the practice makes perfect, The ball went through the hoop, She dropped her ice cream when The screamy cat was very hungry
+
+## Spelling Week 7:
+upset, crawl, shovel, monster, flavour, biscuit, spider, sprinkled, shore, close to the ground, a small creature with eight thin legs that catches insects in a web, legs that catches insects in a, a small chuck tree made from flour, had biscuits with my tea
+
+## Dictation Words Week 7:
+to unhappy or angry, very close to the ground, to move slowly with the body close to the ground, He had to shovel his way through the snow, He was upset when her friend went into the wall, a small creature with eight thin legs that catches insects in a web, She screamed when she saw the spider, The agents were not scared of the monster under the bridge, Chocolate is my favourite flavour, I had biscuits with my tea
+
+## Spelling Week 8:
+sailing, raining, stirred, emerged, puzzled, slashed, confused, became surprised or alarmed, noise, make a very rapid short series, of rocking or jarring sounds, bounce, to make very angry or furious, She was started by the sudden loud
+
+## Dictation Words Week 8:
+make a very rapid short series of rocking or jarring sounds, She was confused, became surprised or alarmed, to make very angry or furious, noise, She was started by the sudden loud
+
+## Spelling Week 9:
+love and over, menu, vitamins, that I eat, Do to opened, someone is shut, purring that it is easy for, wonders why, was thrilled to earn his levels, oil came and eat in his levels, the mother would read her bedtime stories before she slept, the crackers are stored, or the, She will
+
+## Dictation Words Week 9:
+Before Rita goes to bed, her mother often makes some supper for her, She will usually have some chips or fruit with a glass of milk, After that, the mother would read her bedtime stories before she slept, the crackers are stored or the She will
+
+## Spelling Week 10:
+raining, make a very rapid short series of rocking or jarring sounds, be surprised or alarmed, She was started by the sudden loud noise, rolled
+
+## Dictation Words Week 10:
+How could they put the vibriks dark room?, tinned the boy, He paced around the room and searched the box on his sister's face, He whistled while the rat crouched down and searched the box on his sister's face, He
+
+
+
+
+*/
+
+
+
+
+/*
 Set A (least difficult):
 bell, roar, noise, softly, head, hungry, stall, learn, help, then, tide, snail, crab, same, most, float, raft, grew, strong, life, flowers, hot, wind
 */
